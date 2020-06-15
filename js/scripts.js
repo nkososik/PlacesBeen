14b951164747e4df348d5a5b1f548ae2b5ae3a94
Nikolas Kososik <nikolas.kososik@gmail.com>
2020-06-15 14:54:38 -0700

    finalize user interface so object properties show on click of city name in index.html
    Co-Authored-By: James Henager <jameschenager@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -23,10 +23,25 @@ Place.prototype.iLove = function() {
   return "I Love " + this.city + "!!";
 }
 
+//pseudo form code
 let placeList = new PlaceList();
-let place = new Place("Portland", "United States", ["St. John's Bridge", "Burnside Skatepark", "Multnomah Falls"], "Spring");
-let place2 = new Place("Seattle", "United States", ["Space Needle", "Pike Place Market", "CHAZ"], "Summer");
-placeList.addPlace(place);
+let place1 = new Place("Portland", "United States", ["St. John's Bridge", " Burnside Skatepark", " Multnomah Falls"], "Spring");
+let place2 = new Place("Seattle", "United States", ["Space Needle", " Pike Place Market", " CHAZ"], "Summer");
+placeList.addPlace(place1);
 placeList.addPlace(place2);
 
 // User Interface Logic
+$(document).ready(function() {
+  $("#place1").click(function() {
+    $("#placeProperties").append("<li>" + place1.city +"</li>");
+    $("#placeProperties").append("<li>" + place1.country +"</li>");
+    $("#placeProperties").append("<li>" + "Landmarks: " + place1.landmarks +"</li>");
+    $("#placeProperties").append("<li>" + place1.season +"</li>");
+  });
+  $("#place2").click(function() {
+    $("#placeProperties").append("<li>" + place2.city +"</li>");
+    $("#placeProperties").append("<li>" + place2.country +"</li>");
+    $("#placeProperties").append("<li>" + "Landmarks: " + place2.landmarks +"</li>");
+    $("#placeProperties").append("<li>" + place2.season +"</li>");
+  });
+});
